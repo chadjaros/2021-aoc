9bf8e8efb6031dda1c7c8f6c1d37cee83ec26861
Chad Jaros <chad.jaros@gmail.com>
2022-01-04 21:50:33 -0600

    2015 day 12

---
+++ src/2015/12/b.ts
@@ -0,0 +1,29 @@
+import { input12 } from './input';
+
+const input = input12;
+
+const count = (json: unknown): number => {
+    if (typeof json === 'number') {
+        return json as number;
+    }
+    else if (Array.isArray(json)) {
+        const arr = json as unknown[];
+        return arr.reduce<number>((accum, item) => {
+            return accum + count(item);
+        }, 0);
+    }
+    else if (typeof json === 'object') {
+        const obj = json as Record<string, unknown>;
+        if (Object.values(obj).some((v) => v === 'red')) {
+            return 0;
+        }
+        return Object.values(obj).reduce<number>((accum, item) => {
+            return accum + count(item);
+        }, 0);
+    }
+    else {
+        return 0;
+    }
+};
+
+console.log(count(input));
