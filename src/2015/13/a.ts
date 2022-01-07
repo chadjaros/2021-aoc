78483d9d2c6ae815cb7e144f8e0c8ac3f2ccd9da
Chad Jaros <chad.jaros@gmail.com>
2022-01-07 09:36:48 -0600

    2015 day 13

---
+++ src/2015/13/a.ts
@@ -0,0 +1,43 @@
+import { Node } from '../../utils/graph';
+import { input13 } from './input';
+
+const input = input13;
+
+const nodes = input.reduce((acc, value) => {
+    if (!acc.has(value.root)) {
+        acc.set(value.root, {
+            id: value.root,
+            edges: []
+        });
+    }
+    acc.get(value.root)?.edges.push(value);
+    
+    return acc;
+}, new Map<string, Node>());
+
+function search(node: Node, path: Node[], seen: Set<string>, graph: Map<string, Node>): number {
+    if (seen.has(node.id)) {
+        return -Infinity;
+    }
+    
+    if (node.edges.every((x) => seen.has(x.nodeId))) {
+        path.push(node);
+
+        return path.reduce((acc, node, idx) => {
+            const leftIdx = idx - 1 < 0 ? path.length - 1 : idx - 1;
+            const leftWeight = node.edges.find((e) => e.nodeId === path[leftIdx].id)?.weight;
+            const rightIdx = idx + 1 >= path.length ? 0 : idx + 1;
+            const rightWeight = node.edges.find((e) => e.nodeId === path[rightIdx].id)?.weight;
+
+            return acc + leftWeight! + rightWeight!;
+        }, 0);
+    }
+
+    return Math.max(...node.edges.map((e) => {
+        const edge = graph.get(e.nodeId)!;
+        return search(edge, [...path, node], new Set([...seen.values(), node.id]), graph);
+    }));
+}
+
+const start = nodes.values().next().value as Node;
+console.log(search(start, [], new Set(), nodes));
