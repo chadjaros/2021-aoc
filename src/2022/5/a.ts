dff5cab629d92af0c68232b92787c8e85e81ccc6
Chad Jaros <chad.jaros@gmail.com>
2022-12-05 00:02:37 -0600

    2022 day 5

---
+++ src/2022/5/a.ts
@@ -0,0 +1,43 @@
+import { readFileSync } from 'fs';
+import { aoc } from '../../utils/aoc';
+
+aoc(() => {
+    let value = '';
+    const input = readFileSync(__dirname + '/input.txt').toString()
+        .split('\n');
+
+    const stacks: string[][] = [];
+    for (let i = 0; i < 9; i++) {
+        stacks.push([]);
+    }
+    for (const line of input.slice(0, 8)) {
+        for (let i = 0; i < 9; i++) {
+            if (line[1 + i*4] != ' ') {
+                stacks[i].unshift(line[1 + i*4]);
+            }
+        }
+    }
+    
+    const instructions = input.slice(10).map((x) => {
+        const splits = x.split(' ');
+        return [parseInt(splits[1]), parseInt(splits[3]), parseInt(splits[5])];
+    });
+
+    for (let i = 0; i < 9; i++) {
+        console.log(i+1, stacks[i].join(''));
+    }
+
+    for (const ins of instructions) {
+
+        for (let i = 0; i < ins[0]; i++) {
+            stacks[ins[2] - 1].push(stacks[ins[1] - 1].pop()!);
+        }
+    }
+
+    for (let i = 0; i < 9; i++) {
+        value += stacks[i][stacks[i].length - 1];
+    }
+    return {
+        value
+    };
+});
--- src/utils/aoc.ts
+++ src/utils/aoc.ts
@@ -1,12 +1,16 @@
+import { performance } from 'perf_hooks';
+
 export interface AocResult {
     value: number | string;
 }
 
 export const aoc = (f: () => AocResult | Promise<AocResult>): void => {
 
-    const start = Date.now();
+    let start = 0;
+    let end = 0;
     new Promise<AocResult>((res, rej) => {
         try {
+            start = performance.now();
             res(f());
         }
         catch (err) {
@@ -14,17 +18,19 @@ export const aoc = (f: () => AocResult | Promise<AocResult>): void => {
         }
     })
         .then((r) => {
+            end = performance.now();
             const {value, ...extra} = r;
             console.log('---');
             console.log('value:', value);
             console.log('extra:', extra);
         })
         .catch((err) => {
+            end = performance.now();
             console.log('---');
             console.log('error:', err);
         })
         .finally(() => {
             console.log('---');
-            console.log('time: ', Date.now() - start, 'ms');
+            console.log(`time: ${(end - start).toFixed(3)} ms`);
         });
 };
