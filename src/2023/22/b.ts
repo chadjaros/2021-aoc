ceaa5de8512e6e0369e7937e7476448b0d75fe7a
Chad Jaros <chad.jaros@gmail.com>
2024-01-03 20:09:37 -0600

    2023 23a, 22b

--- src/2023/22/b.ts
+++ src/2023/22/b.ts
@@ -1,7 +1,79 @@
 import { aoc } from '../../ts-utils/aoc';
+import { BoundingBox3 } from '../../ts-utils/point-3d';
+import { ArrayUtils } from '../../ts-utils/array-utils';
+
+interface StackNode {
+    bbox: BoundingBox3;
+    supportedBy: string[];
+    supports: string[];
+}
 
 aoc((infile) => {
-    const value = infile.lines[0].length;
+    const boxes = infile.lines.map((line) => {
+        const coords = line.split('~').flatMap((_) => _.split(',').map((_2) => parseInt(_2))) as [number, number, number, number, number, number];
+        return BoundingBox3.fromCoordinates(...coords);
+    });
+    boxes.sort((a, b) => a.max.z - b.max.z);
+
+    const biggest = boxes.reduce((acc, b) => Math.max(acc, ...[b.max.x, b.max.y, b.max.z, b.min.x, b.min.y, b.min.z].map((_) => Math.abs(_))), 0);
+    const base = BoundingBox3.fromCoordinates(-biggest, -biggest, 0, biggest, biggest, 0);
+
+    const stack: StackNode[] = [{ bbox: base, supportedBy: [], supports: [] }];
+    for (const box of boxes) {
+        const proj = box.xyProjection();
+        const matches = stack.reduceRight<StackNode[]>((acc, st) => {
+            const maxHeight = acc[0]?.bbox.max.z ?? -1;
+            if (st.bbox.xyProjection().pointsIntersect(proj)) {
+                if (st.bbox.max.z > maxHeight) {
+                    return [st];
+                }
+                else if (st.bbox.max.z === maxHeight) {
+                    acc.push(st);
+                }
+            }
+            return acc;
+        }, []);
+
+        const maxHeight = matches[0].bbox.max.z;
+        const adjustZ = maxHeight - box.min.z + 1;
+        const bbox = BoundingBox3.fromCoordinates(
+            box.min.x,
+            box.min.y,
+            box.min.z + adjustZ,
+            box.max.x,
+            box.max.y,
+            box.max.z + adjustZ
+        );
+        stack.push({
+            bbox,
+            supportedBy: matches.map((_) => _.bbox.key),
+            supports: [],
+        });
+        matches.forEach((_) => _.supports.push(bbox.key));
+    }
+
+    const stackMap = new Map(stack.map((_) => [_.bbox.key, _]));
+
+    const value = ArrayUtils.tail(stack).reduce((acc, box) => {
+
+        const gone = new Set<string>([box.bbox.key]);
+        const localStack = [...box.supports];
+
+        while (localStack.length !== 0) {
+            const key = localStack.shift()!;
+            if (gone.has(key)) {
+                continue;
+            }
+            const current = stackMap.get(key)!;
+
+            if (current.supportedBy.every((_) => gone.has(_))) {
+                gone.add(key);
+                localStack.push(...current.supports);
+            }
+        }
+
+        return acc + gone.size - 1;
+    }, 0);
 
     return { value };
 });
--- src/2023/23/a.ts
+++ src/2023/23/a.ts
@@ -1,7 +1,110 @@
 import { aoc } from '../../ts-utils/aoc';
+import { Edge, Node, SimpleGraph } from '../../ts-utils/graph';
+import { Grid } from '../../ts-utils/grid';
+import { Point } from '../../ts-utils/point-2d';
+
+// class CompressedNode implements Node {
+//     constructor(readonly point: Point) { }
+
+//     get id(): string {
+//         return this.point.key;
+//     }
+
+//     readonly edges: Edge[];
+
+// }
+
+class BFSNode implements Node {
+    constructor(
+        readonly point: Point,
+        readonly steps: number,
+        readonly seen: Set<string>,
+        readonly grid: Grid<string>,
+        readonly graph: SimpleGraph<BFSNode>
+    ) {
+        graph.setNode(this);
+        this.seen.add(this.id);
+    }
+
+    get id(): string {
+        return this.point.key;
+    }
+
+    get edges(): Edge[] {
+        return this.grid.nodeAt(this.point).edges
+            .filter((gridEdge) => !this.seen.has(gridEdge.nodeId))
+            .map((gridEdge) => {
+                const gridNode = this.grid.getNode(gridEdge.nodeId);
+
+                const nnode = new BFSNode(gridNode.point, this.steps + 1, new Set(this.seen), this.grid, this.graph);
+
+                return {
+                    nodeId: nnode.id,
+                    weight: 1,
+                };
+            });
+    }
+}
 
 aoc((infile) => {
-    const value = infile.lines[0].length;
+    const grid: Grid = infile.grid(undefined, undefined, (p, g) => {
+        const val = g.getValue(p);
+        if (val === '#') {
+            return [];
+        }
+        let adj = p.adjacents().filter((ap) => grid.isValid(ap) && grid.getValue(ap) !== '#');
+        if (val === '>') {
+            adj = adj.filter((ap) => ap.equals(Point.Cardinals.Right.plus(p)));
+        } else if (val === 'v') {
+            adj = adj.filter((ap) => ap.equals(Point.Cardinals.Down.plus(p)));
+        } else if (val === '<') {
+            adj = adj.filter((ap) => ap.equals(Point.Cardinals.Left.plus(p)));
+        } else if (val === '^') {
+            adj = adj.filter((ap) => ap.equals(Point.Cardinals.Up.plus(p)));
+        }
+        return adj.map((ap) => ({
+            nodeId: ap.key,
+            weight: 1,
+        }));
+    });
+
+    // const start = new CompressedNode(grid.scanIncXFrom(new Point(0, 0), (v) => v === '.')!);
+    // const end = new CompressedNode(grid.scanIncXFrom(new Point(0, grid.height - 1), (v) => v === '.')!);
+    // const compressed = new SimpleGraph([start, end]);
+
+    // const seen = new Set<string>();
+    // const stack = [{from: start.point, at: start.point, seen: new Set<string>()}];
+    // while(stack.length > 0) {
+    //     const current = stack.shift()!;
+
+    //     if (seen.has(current.at.key)) {
+    //         continue;
+    //     }
+    //     seen.add(current.at.key);
+
+    // }
+
+    const start = grid.scanIncXFrom(new Point(0, 0), (v) => v === '.')!;
+    const end = grid.scanIncXFrom(new Point(0, grid.height - 1), (v) => v === '.')!;
+
+    const graph = new SimpleGraph<BFSNode>();
+    const stack = [...new BFSNode(start, 0, new Set(), grid, graph).edges];
+    const ends: BFSNode[] = [];
+    while (stack.length > 0) {
+        const edge = stack.shift()!;
+        const current = graph.getNode(edge.nodeId);
+        if (current.point.equals(end)) {
+            ends.push(current);
+            continue;
+        }
+
+        const edges = current.edges;
+        // console.log(edge.nodeId, current.steps, edges);
+        stack.push(...edges);
+    }
+
+    const value = ends.reduce((acc, _) => Math.max(_.steps, acc), 0);
+    console.log(ends.map((_) => _.steps));
 
     return { value };
 });
